8d24d1b7a9f2d15ecbf0275aafdb3f2a74e43bea
Jaspreet2105 <jaspreetk767@gmail.com>
2020-08-05 17:31:40 -0400

    Add access request table

--- ArtistManagementProject(Group_CSAT_0096_1)/AccessRequest.aspx
+++ ArtistManagementProject(Group_CSAT_0096_1)/AccessRequest.aspx
@@ -2,15 +2,37 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div>
+    
        <br/>
     <br/>
     <h1 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">ACCESS REQUESTS </h1>  
+    <div id="contentDiv">
+
+        <div class="row">
+            <div class="col-md-6 offset-6">
+                <%--<button value="Create" name="btnCreate" onclick="alert('Create')"></button>--%>
+                <asp:LinkButton PostBackUrl="~/AddUser.aspx" ID="btn_Created" runat="server" CssClass="btn btn-small btn-primary"><i class="fas fa-plus-circle"></i>&nbsp;Create</asp:LinkButton>
+                <button id="btnEdit" class="btn btn-small btn-primary" onclick="return EditPage()"><i class="fas fa-pencil-alt"></i>&nbsp;Edit</button>
+            </div>
+        </div>
+        <br />
+        <br />
+        <div class="row">
+            <div class="col-md-12">
+                <asp:PlaceHolder ID="PlaceHolder1" runat="server" />
+                <asp:PlaceHolder ID="UserAdded" runat="server" />
+            </div>
+        </div>
     </div>
     <script>
         $(document).ready(function () {
             $('.sidebar').find('.active').eq(0).removeClass('active');
             $('#linkAccessRequest').addClass("active");
+            var table = $('#myTable').DataTable({
+                select: {
+                    style: 'multi'
+                }
+            });
         });
 
     </script>
--- ArtistManagementProject(Group_CSAT_0096_1)/AccessRequest.aspx.cs
+++ ArtistManagementProject(Group_CSAT_0096_1)/AccessRequest.aspx.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,9 +13,73 @@ namespace ArtistManagementProject_Group_CSAT_0096_1_
 {
     public partial class WebForm9 : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataTable dt = new DataTable();
+            dt = UsersData();
 
+            //Building an HTML string.
+            StringBuilder html = new StringBuilder();
+
+            //Table start.
+            html.Append("<div class='card-body'>");
+            html.Append("<div class='table-responsive'>");
+            html.Append("<table class='table table-bordered' width='100%' cellspacing='0' id='myTable' border = '1'>");
+
+            //Building the Header row.
+            html.Append("<thead>");
+            html.Append("<tr>");
+            foreach (DataColumn column in dt.Columns)
+            {
+                html.Append("<th>");
+                html.Append(column.ColumnName);
+                html.Append("</th>");
+            }
+            html.Append("</tr>");
+            html.Append("</thead>");
+            html.Append("<tbody>");
+            //Building the Data rows.
+            foreach (DataRow row in dt.Rows)
+            {
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<td>");
+                    html.Append(row[column.ColumnName]);
+                    html.Append("</td>");
+                }
+                html.Append("</tr>");
+            }
+            html.Append("</tbody>");
+
+            //Table end.
+            html.Append("</table>");
+            html.Append("</div>");
+            html.Append("</div>");
+            //html.Append("<script>");
+            //html.Append("$(document).ready(function () {$('#myTable').DataTable();}); ");
+            //html.Append("</script>");
+
+            //Append the HTML string to Placeholder.
+            PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+        }
+        private DataTable UsersData()
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection(strcon);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            string query = "select * from accessrequest ";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds);
+            con.Close();
+            return ds.Tables[0];
         }
     }
 }
